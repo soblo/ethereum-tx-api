fa3ba221b79f55c5b8f8d3753efcea9b2eedcf01
이성일 <modolee@soblo.io>
2018-11-19 21:30:48 +0900

    config dir replaced with .env file

--- config/connection.example.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rpc : {
-    dev : 'http://localhost:7545',
-    production : '',
-  },
-  mnemonic : '',
-}
--- helpers/web3Initializer.js
+++ helpers/web3Initializer.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // for configuration
+
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const connection = require('../config/connection');
 
-const provider = new HDWalletProvider(connection.mnemonic, connection.rpc.dev);
+const provider = new HDWalletProvider(process.env.MNEMONIC_WORDS, process.env.RPC_ADDRESS);
 const web3 = new Web3(provider);
 
 provider.engine.stop();
